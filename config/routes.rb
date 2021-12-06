35fc3f7f81b2f8ea1211014539825bc2d85eaa65
ugo lahaye <lahaye.ugo@hotmail.fr>
2021-12-06 18:46:38 +0100

    next

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-    get 'about', to: 'pages#about'
-    get 'competences', to: 'pages#competences'
-    get 'materiel', to: 'pages#materiel'
+    get 'about',         to: 'pages#about'
+    get 'competences',   to: 'pages#competences'
+    get 'materiel',      to: 'pages#materiel'
+    get 'renseignement', to: 'pages#renseignement'
+    get 'devis',         to: 'pages#devis'
 end
