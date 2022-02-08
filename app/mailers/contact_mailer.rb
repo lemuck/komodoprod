0d29682f55a66f22072dc20028455a552d40bee5
ugo lahaye <lahaye.ugo@hotmail.fr>
2022-02-08 09:48:53 +0100

    contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
-# class ContactMailer < ApplicationMailer
-#   default from: "from@example.com"
-#   def contact(message)
-#     @message = message
-#     mail(to: 'hello@komodoprod.com', subject: 'Test')
-#   end
-# end
+class ContactMailer < ApplicationMailer
+  default from: "hello@komodoprod.com"
+  def contact(message)
+    @message = message
+    mail(to: 'hello@komodoprod.com', subject: 'Test')
+  end
+end
