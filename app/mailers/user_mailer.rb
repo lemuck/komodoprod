10194ea2d3ceb2f71aceed280bcab9ddd704b2fb
ugo lahaye <lahaye.ugo@hotmail.fr>
2022-02-08 09:53:54 +0100

    ...

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
-# class UserMailer < ApplicationMailer
-#   default from: 'hello@komodoprod.com'
+class UserMailer < ApplicationMailer
+  default from: 'hello@komodoprod.com'
 
-#   def welcome_email
-#     @user = params[:user]
-#     @url  = 'http://komodoprod.com'
-#     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-#   end
-# end
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://komodoprod.com'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
